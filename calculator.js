c96636d340fb7626bb864096bb98edafc297df6a
Kwok Wai Ting <waitingkwok2@ln.hk>
2024-02-02 17:39:24 +0800

    Cal

--- calculator.js
+++ calculator.js
@@ -1,11 +1,29 @@
+window.onload = function() {
+    let buttons = document.getElementsByClassName('number');
+    let display = document.getElementById('displayValue');
+
+    buttons.map( button => {
+        button.addEventListener('click', (e) => {
+            if (display.innerHTML === '0') {
+                display.innerHTML = e.target.value;  // this will replace the initial 0 in the display
+            } else {
+                display.innerHTML += e.target.value; // this will add the clicked number to the display
+            }
+        });
+    });
+};
+
 function digitOnClick() {
+    let Digit = document.getElementById('displayValue').innerHTML;
+    Digit += this.innerHTML;
+    alert(Digit);
     console.log(this);
 }
 
 function operateOnClick() {
     //alert(this.classList);
     let OperateBtns = document.getElementsByClassName('operate');
-    for ( let loop=0; loop<OperateBtns.length; loop++){
+    for ( let loop=0; loop<OperateBtns.length; loop++ ){
         OperateBtns[loop].classList.remove("operateClick")
     };
     this.classList.add("operateClick");
@@ -14,7 +32,7 @@ function operateOnClick() {
 }
 
 function equalOnClick() {
-    
+
 }
 
 function cancelOnClick() {
@@ -23,10 +41,10 @@ function cancelOnClick() {
 
 document.addEventListener('DOMContentLoaded', function() 
 {
-    let digitBtns = document.getElementsByClassName('digit');
+    let DigitBtns = document.getElementsByClassName('digit');
 
-    for ( let i=0; i<digitBtns.length; i++ ) {
-        digitBtns[i].addEventListener('click', digitOnClick);
+    for ( let i=0; i<DigitBtns.length; i++ ) {
+        DigitBtns[i].addEventListener('click', digitOnClick);
     }
 })
 
