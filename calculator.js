66e6f684c4079f499d8c0d281efe91f0c3c1d855
Kwok Wai Ting <waitingkwok2@ln.hk>
2024-02-02 18:21:45 +0800

    Text change

--- calculator.js
+++ calculator.js
@@ -1,23 +1,6 @@
-window.onload = function() {
-    let buttons = document.getElementsByClassName('number');
-    let display = document.getElementById('displayValue');
-
-    buttons.map( button => {
-        button.addEventListener('click', (e) => {
-            if (display.innerHTML === '0') {
-                display.innerHTML = e.target.value;  // this will replace the initial 0 in the display
-            } else {
-                display.innerHTML += e.target.value; // this will add the clicked number to the display
-            }
-        });
-    });
-};
-
 function digitOnClick() {
-    let Digit = document.getElementById('displayValue').innerHTML;
-    Digit += this.innerHTML;
-    alert(Digit);
-    console.log(this);
+    displayValue.textContent += this.textContent;
+    alert(displayValue);
 }
 
 function operateOnClick() {
@@ -27,7 +10,6 @@ function operateOnClick() {
         OperateBtns[loop].classList.remove("operateClick")
     };
     this.classList.add("operateClick");
-    
     console.log(this);
 }
 
