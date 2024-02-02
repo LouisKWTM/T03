b65f9f0f1b9deecd1f4d34b23be68de3c7f075cb
Kwok Wai Ting <waitingkwok2@ln.hk>
2024-02-02 17:04:48 +0800

    Latest

--- calculator.js
+++ calculator.js
@@ -1,17 +1,32 @@
 function digitOnClick() {
-    console.log(this)
+    console.log(this);
 }
 
-function operateOnClick() {}
+function operateOnClick() {
+    //alert(this.classList);
+    let OperateBtns = document.getElementsByClassName('operate');
+    for ( let loop=0; loop<OperateBtns.length; loop++){
+        OperateBtns[loop].classList.remove("operateClick")
+    };
+    this.classList.add("operateClick");
+    
+    console.log(this);
+}
 
-function equalOnClick() {}
+function equalOnClick() {
+    
+}
+
+function cancelOnClick() {
+    document.querySelector("#displayValue").value == document.body.innerHTML.replace(document.querySelector("#displayValue").value, '123456');
+}
 
 document.addEventListener('DOMContentLoaded', function() 
 {
     let digitBtns = document.getElementsByClassName('digit');
 
     for ( let i=0; i<digitBtns.length; i++ ) {
-        digitBtns[i].addEventListener('click', digitOnClick)
+        digitBtns[i].addEventListener('click', digitOnClick);
     }
 })
 
@@ -20,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function()
     let OperateBtns = document.getElementsByClassName('operate');
 
     for ( let i=0; i<OperateBtns.length; i++ ) {
-        OperateBtns[i].addEventListener('click', digitOnClick)
+        OperateBtns[i].addEventListener('click', operateOnClick);
     }
 })
 
@@ -31,5 +46,5 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
     let CancelButton = document.querySelector('#cancel');
-    CancelButton.addEventListener('click', digitOnClick)
+    CancelButton.addEventListener('click', cancelOnClick)
 })
